1bbe178f4e116392bcbb64a00e1f1ea9442b9198
devsetgo <mikeryan56@gmail.com>
2019-11-02 14:42:44 -0400

    adding to document all libraries and their dependencies

--- app/scripts/tests.sh
+++ app/scripts/tests.sh
@@ -15,5 +15,9 @@ python3 -m pytest -v -s
 # create coverage-badge
 coverage-badge -o ../coverage.svg -f
 
+# document all libraries and their dependencies
+pip3 freeze > requirements/all_libraries_used.txt
+
 # generate flake8 report
 flake8 --tee . > flake8_report/report.txt
+
