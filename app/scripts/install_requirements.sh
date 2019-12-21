147bf13061470f9fc7d95022add06222af657662
mike ryan <mikeryan56@gmail.com>
2019-12-21 08:17:22 -0500

    adding upgrade during installl

--- app/scripts/install_requirements.sh
+++ app/scripts/install_requirements.sh
@@ -2,11 +2,7 @@
 set -e
 set -x
 
-# remove current libraries
-pip3 uninstall -r requirements/dev.txt -y
-
 # upgrade pip and setuptools
 pip3 install --upgrade pip setuptools
-
 # install requirements
 pip3 install -r requirements/dev.txt
